9f628c5a128f0bb022f473fd97421aa78cff1ff0
Alex Mir <expressprogress@ya.ru>
2021-07-17 21:56:02 +0300

    feat: changing header data with Routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,34 @@
-import { Link } from 'react-router-dom'
+import { Link, Switch, Route } from 'react-router-dom'
 import headerLogo from '../images/header/header__logo.svg'
-import React, { useEffect } from 'react'
+import React from 'react'
 export default function Header(props) {
-  const signUpBtnData = {
-    handler: function () {},
-    historyPath: '/signup',
-    text: 'Регистрация'
-  }
-  const signInBtnData = {
-    handler: function () {},
-    historyPath: '/signin',
-    text: 'Войти'
-  }
-  const signedInBtnData = {
-    handler: function () {
-      localStorage.removeItem('token')
-    },
-    historyPath: '/signin',
-    text: 'Выйти'
-  }
-  useEffect(() => {
-    if (props.history.location.pathname == '/') {
-      props.setHeaderBtnData(signedInBtnData)
-    }
-    if (props.history.location.pathname == '/signin') {
-      props.setHeaderBtnData(signUpBtnData)
-    }
-    if (props.history.location.pathname == '/signup') {
-      props.setHeaderBtnData(signInBtnData)
-    }
-  }, [])
-
   return (
     <header className='header'>
       <img src={headerLogo} alt='Логотип Места' className='header__logo' />
       <div className='header__verification-container'>
-        {props.headerBtnData && (
-          <>
+        <Switch>
+          <Route path='/signup'>
+            <Link className='header__btn-signin' to='/signin'>
+              Вход
+            </Link>
+          </Route>
+          <Route path='/signin'>
+            <Link className='header__btn-signin' to='/signup'>
+              Регистрация
+            </Link>
+          </Route>
+          <Route path='/'>
             <span className='header__user-email'>{props.userEmail.email}</span>
             <Link
-              to={props.headerBtnData.historyPath}
+              className='header__btn-signin'
               onClick={() => {
-                props.headerBtnData.handler()
-                props.userEmail.setUserEmail(null)
+                localStorage.removeItem('token')
               }}
-              className='header__btn-signin'>
-              {props.headerBtnData && props.headerBtnData.text}
+              to='/signin'>
+              Выйти
             </Link>
-          </>
-        )}
+          </Route>
+        </Switch>
       </div>
     </header>
   )

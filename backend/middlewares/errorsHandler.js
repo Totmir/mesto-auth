a761066e9384c242771a4ef4c82c21b17034a676
Alexandr Mir <totfront@gmail.com>
2021-11-09 19:48:12 +0100

    feat: errStatus(500) if there is no errStatus

--- backend/middlewares/errorsHandler.js
+++ backend/middlewares/errorsHandler.js
@@ -1,6 +1,8 @@
 const errorsHandler = (err, req, res, next) => {
   const { message } = err;
-  const status = err.statusCode;
+  let status = err.statusCode;
+
+  if (!status) status = 500
 
   res.status(status).send({ message });
   next();
